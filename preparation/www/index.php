0e2022c6b80074460da0a348f1f1a54d271f5514
Martin Leadbeater <martin@mo.id.au>
2013-06-01 14:21:36 +0930

    more work on the api

--- preparation/www/index.php
+++ preparation/www/index.php
@@ -1,4 +1,5 @@
 <?php
+$INC = "inc";
 $debug = array();
 //$debug[] = getallheaders();
 $debug[] = "request: " . $_SERVER["REQUEST_URI"];
@@ -12,31 +13,60 @@ if (count($params) < 2 || $params[1] == '') {
 	return;
 }
 
+
+$search=array();
+foreach ($params as $p) {
+  if (substr($p,0,7) == 'search?') {
+     $tags = explode(',', substr($p,7));
+     $debug[] .= "search: " . var_export($tags, true);
+     break;
+  }
+}
+
 $results = array();
 
+// Tours
 if (in_array('tours',$params)) {
+
 	$results = array( 
-		array('name' => 'Test1'), 
-		array('name' => 'Test2') 
+		array('id' => 1, 'name' => 'Test1'), 
+		array('id' => 2, 'name' => 'Test2') 
 	) ;
+
 }
 else if (in_array('tour', $params)) {
-	
+	if ($params[2] == 1) {
+		$dummy_tour_desc = file_get_contents("$INC/tour1_desc.html");
+		$results = array( 'id' => 1, 'name' => 'Test1', 'desc' => $dummy_tour_desc, 
+				'address' => '182 Victoria Square, Adelaide, SA 5000', 'lat' => '34.927', 'lon' => '138.602', 'images' => array());
+	}
+	else if ($params[2] == 2) {
+		$dummy_tour_desc = file_get_contents("$INC/tour2_desc.html");
+		$results = array( 'id' => 2, 'name' => 'Test2', 'desc' => $dummy_tour_desc, 
+				'address' => '184 Victoria Square, Adelaide, SA 5000', 'lat' => '34.928', 'lon' => '138.601', 'images' => array());
+	}
+	else
+		$results = array();
 }
 
 if (in_array('json', $params) || array_key_exists('CONTENT_TYPE', $_SERVER) && $_SERVER['CONTENT_TYPE'] == 'application/json') { 
+	header('Content-Type: application/json');
 	print json_encode( $results );
 	return;
 }
 
 // The viewer/debug website
 $display_results = '';
-foreach($results as $msg) $display_results .= "<p>". var_export($msg,true) . "</p>\n";
+foreach($results as $fld => $val) 
+	if (is_array($val))
+		$display_results .= "<p>$fld: ". var_export($val,true) . "</p>\n";
+	else
+		$display_results .= "<p>$fld: ". $val . "</p>\n";
 
 $debug[] = "\nEnvironment variables:";
 $debug[] = $_SERVER;
 $debug_messages = '';
-foreach($debug as $msg) $debug_messages .= var_export($msg, true) . "\n";
+foreach($debug as $val ) $debug_messages .= var_export($val, true) . "\n";
 print <<<EOD
 <html>
 <head>
